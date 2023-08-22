b1aec4e7a5a6bac63ef4a835520da4c74d703fd7
RajuReddyAtla <142866257+RajuReddyAtla@users.noreply.github.com>
2023-08-22 16:34:15 +0530

    rr (#4)

---
+++ Raju/example1/src/main/java/com/crud/example1/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.crud.example1.repository;
+
+import com.crud.example1.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+    Product findByName(String name);
+}
